70da1fa699079a7e5886515947fb2ef84a9a95c4
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-07-29 08:41:55 +0200

    convertorInkToJson

--- src/functions/inkjsCompiler.ts
+++ src/functions/inkjsCompiler.ts
@@ -1,10 +1,13 @@
 import inkjs from "inkjs";
 
-function convertor(test: string) {
+export function convertorInkToJson(test: string): string {
     try {
         const story = new inkjs.Compiler(test).Compile();
-        return story.ToJson();
+        let json = story.ToJson();
+        console.log(json);
+        return json;
     } catch (e) {
+        console.error("[Pixi'VN] Error compiling ink file", e)
         throw e
     }
 }
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './classes';
-export * from './decorators';
-export * from './functions';
-export * from './interface';
-export * from './labels';
-export * from './managers';
-export * from './types';
-
