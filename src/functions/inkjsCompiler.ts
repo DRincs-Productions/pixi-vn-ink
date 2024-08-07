46093703960191c0fba62cea806c4f9c70fe1a61
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-07 10:03:42 +0200

    first test

--- src/functions/inkjsCompiler.ts
+++ src/functions/inkjsCompiler.ts
@@ -24,31 +24,108 @@ type StepLabelJsonType = {
     }
 }
 
-export function getJsonLabels(json: string): { [labelId: string]: StepLabelJsonType[] } {
-    // todo: implement this function
-    // labelId is the name of the label, in ink it is the text between === ===
+type Typew = "done" | { "#n": string }
 
-    // the array of StepLabelJsonType is the list of steps that are in that label
+type Label = {
+    [labelId: string]: (
+        string |
+        '\n' |
+        null |
+        { "->": string }
+    )[]
+}
 
-    // for example, in the label === back_in_london ===
-    // there are two steps:
-    // 1. We arrived into London at 9.45pm exactly.
-    // 2. -> hurry_home
+type Item = Typew | Item[] | Label
 
-    // so the return value should be:
-    // {
-    //     back_in_london: [
-    //         {
-    //             dialog: "We arrived into London at 9.45pm exactly."
-    //         },
-    //         {
-    //             labelToOpen: {
-    //                 labelId: "hurry_home",
-    //                 type: "call"
-    //             }
-    //         }
-    //     ]
-    // }
+type JsonLabelsType = {
+    inkVersion: number,
+    listDefs: {},
+    root: Item[],
+}
 
-    return {};
+function addLabels(item: object, labels: { [labelId: string]: StepLabelJsonType[] }) {
+    // for value and key in item
+    for (const [key, value] of Object.entries(item)) {
+        // if value is an array
+        if (value instanceof Array) {
+            let aaaa: StepLabelJsonType[] = []
+            value.forEach((v) => {
+                if (typeof v === "string") {
+                    if (v !== '\n') {
+                        aaaa.push({
+                            dialog: v
+                        })
+                    }
+                }
+                else {
+                    console.log("ignore", v)
+                }
+            })
+            if (aaaa.length > 0) {
+                labels[key] = aaaa
+            }
+
+        }
+        else {
+            console.log(value)
+        }
+    }
+}
+
+function findLabel(items: Item[], labels: { [labelId: string]: StepLabelJsonType[] }) {
+    for (const item of items) {
+        if (typeof item === "object") {
+            if (item instanceof Array) {
+                findLabel(item, labels)
+            }
+            else if (item === null) {
+            }
+            // is object
+            else if (typeof item === "object") {
+                addLabels(item, labels)
+            }
+            else {
+                console.log(item)
+            }
+        }
+    }
+}
+
+export function getJsonLabels(json: string): { [labelId: string]: StepLabelJsonType[] } | undefined {
+    try {
+        // convert the json string to object
+        let obj: JsonLabelsType = JSON.parse(json);
+        // todo: implement this function
+        // labelId is the name of the label, in ink it is the text between === ===
+
+        // the array of StepLabelJsonType is the list of steps that are in that label
+
+        // for example, in the label === back_in_london ===
+        // there are two steps:
+        // 1. We arrived into London at 9.45pm exactly.
+        // 2. -> hurry_home
+
+        // so the return value should be:
+        // {
+        //     back_in_london: [
+        //         {
+        //             dialog: "We arrived into London at 9.45pm exactly."
+        //         },
+        //         {
+        //             labelToOpen: {
+        //                 labelId: "hurry_home",
+        //                 type: "call"
+        //             }
+        //         }
+        //     ]
+        // }
+
+        let label: { [labelId: string]: StepLabelJsonType[] } = {}
+
+        findLabel(obj.root, label)
+
+        return {};
+    } catch (e) {
+        console.error("[Pixi'VN Ink] Error parsing json", e)
+    }
 }
