2cbcd54a44d6c31c15193d691792fe7788a06ce8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-07 17:02:17 +0200

    add more test

--- choices.test.ts
+++ choices.test.ts
@@ -1,6 +1,9 @@
 import { test } from 'vitest';
 import { convertInkText } from './src/functions';
 
+// Choices
+// https://github.com/inkle/ink/blob/master/Documentation/WritingWithInk.md#2-choices
+
 test('Choices test 1', async () => {
 	let res = convertInkText(`
 === test ===
@@ -10,17 +13,43 @@ Hello world!
 `);
 });
 
-
 test('Choices test 2', async () => {
 	let res = convertInkText(`
 === test ===
 Hello world!
-*	Hello back!
-	Nice to hear from you!
 Hello world!2
-*	Hello back!3
-	Nice to hear from you!3
-*	Hello back!4
-	Nice to hear from you!4
+*	[Hello back!]
+	Nice to hear from you!
+`);
+});
+
+test('Choices test 3', async () => {
+	let res = convertInkText(`
+=== test ===
+Hello world!
+*	Hello [back!] right back to you!
+	Nice to hear from you!
+`);
+});
+
+test('Choices test 4', async () => {
+	let res = convertInkText(`
+=== test ===
+"What's that?" my master asked.
+*	"I am somewhat tired[."]," I repeated.
+	"Really," he responded. "How deleterious."
+`);
+});
+
+test('Choices test 5', async () => {
+	let res = convertInkText(`
+=== test ===
+"What's that?" my master asked.
+*	"I am somewhat tired[."]," I repeated.
+	"Really," he responded. "How deleterious."
+*	"Nothing, Monsieur!"[] I replied.
+	"Very good, then."
+*  "I said, this journey is appalling[."] and I want no more of it."
+	"Ah," he replied, not unkindly. "I see you are feeling frustrated. Tomorrow, things will improve."
 `);
 });
--- errors.test.ts
+++ errors.test.ts
@@ -10,16 +10,6 @@ We arrived into London at 9.45pm exactly.
 
 === hurry_home ===
 We hurried home to Savile Row as fast as we could.
-->DONE
-`);
-});
-
-// TODO: To handle the end of the game I should have the user pass me a functionok
-test('Label test 2', async () => {
-	let res = convertInkText(`
-=== back_in_london ===
-
-We arrived into London at 9.45pm exactly.
--> END
+// ->DONE -> ERROR
 `);
 });
