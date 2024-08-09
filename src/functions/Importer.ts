eb73169198dd2e37dcac9535f14d9c4f34f0464e
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-09 13:40:49 +0200

    Varius fix

--- src/functions/Importer.ts
+++ src/functions/Importer.ts
@@ -1,7 +1,14 @@
 import { importPixiVNJson } from "@drincs/pixi-vn";
 import { convertInkText } from "./InkToPixivn";
 
-export function importInkText(text: string) {
+export function importInkText(text: string | string[]) {
+    // if is array
+    if (Array.isArray(text)) {
+        text.forEach((t) => {
+            importInkText(t)
+        })
+        return
+    }
     let data = convertInkText(text)
     if (data) {
         {
