a2fb092ba4fb69d06134d74e268ffbb77658650d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-22 09:48:04 +0200

    ReadCount

--- src/types/parserItems/RootParserItemType.ts
+++ src/types/parserItems/RootParserItemType.ts
@@ -2,9 +2,10 @@ import ChoicePoint, { ChoiceInfo } from "./ChoicePoint"
 import ControlCommands from "./ControlCommands"
 import Divert from "./Divert"
 import NativeFunctions from "./NativeFunctions"
+import ReadCount from "./ReadCount"
 import TextType from "./TextType"
 import VariableAssignment from "./VariableAssignment"
 import VariableReference from "./VariableReference"
 
-type RootParserItemType = null | RootParserItemType[] | ChoiceInfo | ChoicePoint | ControlCommands | Divert | NativeFunctions | TextType | VariableAssignment | VariableReference
+type RootParserItemType = null | RootParserItemType[] | ChoiceInfo | ChoicePoint | ControlCommands | Divert | NativeFunctions | TextType | VariableAssignment | VariableReference | ReadCount
 export default RootParserItemType
