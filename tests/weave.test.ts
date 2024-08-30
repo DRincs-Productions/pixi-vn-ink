0f4597fe8c66ff70b596df7f939dc8af4e3313bf
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-30 23:23:18 +0200

    Gathers1

---
+++ tests/weave.test.ts
@@ -0,0 +1,28 @@
+import { PixiVNJson } from '@drincs/pixi-vn';
+import { expect, test } from 'vitest';
+import { convertInkText } from '../src/functions';
+
+/**
+ * https://github.com/inkle/ink/blob/master/Documentation/WritingWithInk.md#1-gathers
+ */
+
+test('Gathers1', async () => {
+    let expected: PixiVNJson = {
+        labels: {}
+    }
+    let res = convertInkText(`
+-> start
+=== start ==
+"What's that?" my master asked.
+	*	"I am somewhat tired[."]," I repeated.
+		"Really," he responded. "How deleterious."
+	*	"Nothing, Monsieur!"[] I replied.
+		"Very good, then."
+	*  "I said, this journey is appalling[."] and I want no more of it."
+	"Ah," he replied, not unkindly. "I see you are feeling frustrated. Tomorrow, things will improve."
+
+-	With that Monsieur Fogg left the room.
+-> DONE
+`);
+    expect(res).toEqual(expected);
+});
