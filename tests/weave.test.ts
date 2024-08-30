a8354af068f6257599e8d639a7b91977b4f110f8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-30 23:24:30 +0200

    Options and gathers form chains of content

--- tests/weave.test.ts
+++ tests/weave.test.ts
@@ -5,8 +5,7 @@ import { convertInkText } from '../src/functions';
 /**
  * https://github.com/inkle/ink/blob/master/Documentation/WritingWithInk.md#1-gathers
  */
-
-test('Gathers1', async () => {
+test('Gathers', async () => {
     let expected: PixiVNJson = {
         labels: {}
     }
@@ -22,6 +21,35 @@ test('Gathers1', async () => {
 	"Ah," he replied, not unkindly. "I see you are feeling frustrated. Tomorrow, things will improve."
 
 -	With that Monsieur Fogg left the room.
+-> DONE
+`);
+    expect(res).toEqual(expected);
+});
+
+/**
+ * https://github.com/inkle/ink/blob/master/Documentation/WritingWithInk.md#options-and-gathers-form-chains-of-content
+ */
+test('Options and gathers form chains of content', async () => {
+    let expected: PixiVNJson = {
+        labels: {}
+    }
+    let res = convertInkText(`
+-> start
+=== start ==
+I ran through the forest, the dogs snapping at my heels.
+
+	* 	I checked the jewels[] were still in my pocket, and the feel of them brought a spring to my step. <>
+
+	*  I did not pause for breath[] but kept on running. <>
+
+	*	I cheered with joy. <>
+
+- 	The road could not be much further! Mackie would have the engine running, and then I'd be safe.
+
+	*	I reached the road and looked about[]. And would you believe it?
+	* 	I should interrupt to say Mackie is normally very reliable[]. He's never once let me down. Or rather, never once, previously to that night.
+
+-	The road was empty. Mackie was nowhere to be seen.
 -> DONE
 `);
     expect(res).toEqual(expected);
