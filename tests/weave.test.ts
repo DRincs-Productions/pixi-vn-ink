1e36e841f1ae0c5e054d363d9a8caf9934200d2a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-08-31 17:46:30 +0200

    Gathers

--- tests/weave.test.ts
+++ tests/weave.test.ts
@@ -6,10 +6,109 @@ import { convertInkText } from '../src/functions';
  * https://github.com/inkle/ink/blob/master/Documentation/WritingWithInk.md#1-gathers
  */
 test('Gathers', async () => {
-    let expected: PixiVNJson = {
-        labels: {}
-    }
-    let res = convertInkText(`
+	let expected: PixiVNJson = {
+		labels: {
+			"start_|_c-0": [
+				{
+					dialogue: "\"I am somewhat tired",
+					glueEnabled: true,
+					goNextStep: true,
+				},
+				{
+					dialogue: ",\" I repeated.",
+				},
+				{
+					dialogue: "\"Really,\" he responded. \"How deleterious.\"",
+				},
+				{
+					labelToOpen: {
+						label: "start_|_g-0",
+						type: "call",
+					},
+					glueEnabled: undefined,
+				},
+			],
+			"start_|_c-1": [
+				{
+					dialogue: "\"Nothing, Monsieur!\"",
+					glueEnabled: true,
+					goNextStep: true,
+				},
+				{
+					dialogue: " I replied.",
+				},
+				{
+					dialogue: "\"Very good, then.\"",
+				},
+				{
+					labelToOpen: {
+						label: "start_|_g-0",
+						type: "call",
+					},
+					glueEnabled: undefined,
+				},
+			],
+			"start_|_c-2": [
+				{
+					dialogue: "\"I said, this journey is appalling",
+					glueEnabled: true,
+					goNextStep: true,
+				},
+				{
+					dialogue: " and I want no more of it.\"",
+				},
+				{
+					dialogue: "\"Ah,\" he replied, not unkindly. \"I see you are feeling frustrated. Tomorrow, things will improve.\"",
+				},
+				{
+					labelToOpen: {
+						label: "start_|_g-0",
+						type: "call",
+					},
+					glueEnabled: undefined,
+				},
+			],
+			"start_|_g-0": [
+				{
+					dialogue: "With that Monsieur Fogg left the room.",
+				},
+				{
+					end: "label_end",
+				},
+			],
+			start: [
+				{
+					dialogue: "\"What's that?\" my master asked.",
+				},
+				{
+					choices: [
+						{
+							text: ["\"I am somewhat tired", ".\"",],
+							label: "start_|_c-0",
+							props: {},
+							type: "call",
+							oneTime: true,
+						},
+						{
+							text: "\"Nothing, Monsieur!\"",
+							label: "start_|_c-1",
+							props: {},
+							type: "call",
+							oneTime: true,
+						},
+						{
+							text: ["\"I said, this journey is appalling", ".\"",],
+							label: "start_|_c-2",
+							props: {},
+							type: "call",
+							oneTime: true,
+						},
+					],
+				},
+			],
+		}
+	}
+	let res = convertInkText(`
 -> start
 === start ==
 "What's that?" my master asked.
@@ -23,17 +122,17 @@ test('Gathers', async () => {
 -	With that Monsieur Fogg left the room.
 -> DONE
 `);
-    expect(res).toEqual(expected);
+	expect(res).toEqual(expected);
 });
 
 /**
  * https://github.com/inkle/ink/blob/master/Documentation/WritingWithInk.md#options-and-gathers-form-chains-of-content
  */
 test('Options and gathers form chains of content', async () => {
-    let expected: PixiVNJson = {
-        labels: {}
-    }
-    let res = convertInkText(`
+	let expected: PixiVNJson = {
+		labels: {}
+	}
+	let res = convertInkText(`
 -> start
 === start ==
 I ran through the forest, the dogs snapping at my heels.
@@ -52,5 +151,5 @@ I ran through the forest, the dogs snapping at my heels.
 -	The road was empty. Mackie was nowhere to be seen.
 -> DONE
 `);
-    expect(res).toEqual(expected);
+	expect(res).toEqual(expected);
 });
