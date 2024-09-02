425a759a63c2ca687280db762762fd972f797b6b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-09-02 23:03:36 +0200

    Defining Global Variables

--- tests/variables.test.ts
+++ tests/variables.test.ts
@@ -18,6 +18,9 @@ VAR knowledge_of_the_cure = false
 VAR players_name = "Emilia"
 VAR number_of_infected_people = 521
 VAR current_epilogue = -> they_all_die_of_the_plague
+
+=== they_all_die_of_the_plague
+-> DONE
 `);
     expect(res).toEqual(expected);
 });
