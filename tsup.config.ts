c7bf8295e20d5a89fc5ab1d78f963cfd7fdd62bb
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-09-22 17:29:18 +0200

    edit tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "tsup";
 
+// https://dev.to/orabazu/how-to-bundle-a-tree-shakable-typescript-library-with-tsup-and-publish-with-npm-3c46
 export default defineConfig({
-    entry: ["src/**/*@(ts|tsx)"],
+    entry: ["src/**/*.ts"],
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
     treeshake: true,
     splitting: false,
-    sourcemap: true,
+    // sourcemap: true, // Generate sourcemap, it was removed because otherwise it would explode
     clean: true,
+    minify: true,
+    bundle: true,
+    skipNodeModulesBundle: false, // Skip bundling of node_modules
+    entryPoints: ['src/index.ts'],
 });
