16edc548d3076699256aedb62518a55ac300cd1a
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-08-21 18:46:07 +0200

    feat: update package version to 0.9.0 and enhance vite.js export configuration in package.json; modify entry points in tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from "tsup";
 // https://dev.to/orabazu/how-to-bundle-a-tree-shakable-typescript-library-with-tsup-and-publish-with-npm-3c46
 export default defineConfig({
     target: "es2020",
-    entry: ["src/**/index.ts"],
+    entry: {
+        index: "src/index.ts",
+        "vite.js": "src/vite-js/index.ts",
+    },
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
     treeshake: true,
-    splitting: false,
     // sourcemap: true, // Generate sourcemap, it was removed because otherwise it would explode
     clean: true,
     minify: true,
