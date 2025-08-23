c04f07bf51b1e2991b46316bb009ac30c11ae24d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-08-23 22:34:50 +0200

    feat: bump version to 0.9.3 and update @drincs/pixi-vn-json dependency to 1.7.5; enhance game state management in noHmrInkPlugin

--- src/vite-js/plugins.ts
+++ src/vite-js/plugins.ts
@@ -1,3 +1,8 @@
+import * as characterUtils from "@drincs/pixi-vn/characters";
+import * as historyUtils from "@drincs/pixi-vn/history";
+import * as narrationUtils from "@drincs/pixi-vn/narration";
+import * as storageUtils from "@drincs/pixi-vn/storage";
+import { GameUnifier } from "@drincs/pixi-vn/unifier";
 import { Plugin } from "vite";
 import { importInkText } from "../functions/importer";
 
@@ -12,6 +17,44 @@ export function noHmrInkPlugin(): Plugin {
         async handleHotUpdate({ file, read }) {
             if (file.endsWith(".ink")) {
                 const fileText = await read();
+                GameUnifier.init({
+                    getCurrentGameStepState: () => {
+                        return {
+                            storage: storageUtils.storage.export(),
+                            labelIndex: narrationUtils.NarrationManagerStatic.currentLabelStepIndex || 0,
+                            openedLabels: narrationUtils.narration.openedLabels,
+                        };
+                    },
+                    restoreGameStepState: async (state, navigate) => {
+                        historyUtils.HistoryManagerStatic._originalStepData = state;
+                        narrationUtils.NarrationManagerStatic.openedLabels = state.openedLabels;
+                        storageUtils.storage.restore(state.storage);
+                        navigate(state.path);
+                    },
+                    // narration
+                    getStepCounter: () => narrationUtils.narration.stepCounter,
+                    setStepCounter: (value) => {
+                        narrationUtils.NarrationManagerStatic._stepCounter = value;
+                    },
+                    getOpenedLabels: () => narrationUtils.narration.openedLabels.length,
+                    addHistoryItem: (historyInfo, options) => {
+                        return historyUtils.stepHistory.add(historyInfo, options);
+                    },
+                    getCurrentStepsRunningNumber: () => narrationUtils.NarrationManagerStatic.stepsRunning,
+                    getCharacter: (id: string) => {
+                        return characterUtils.RegisteredCharacters.get(id);
+                    },
+                    // canvas
+                    onGoNextEnd: async () => {},
+                    // storage
+                    getVariable: (key) => storageUtils.storage.getVariable(key),
+                    setVariable: (key, value) => storageUtils.storage.setVariable(key, value),
+                    removeVariable: (key) => storageUtils.storage.removeVariable(key),
+                    getFlag: (key) => storageUtils.storage.getFlag(key),
+                    setFlag: (name, value) => storageUtils.storage.setFlag(name, value),
+                    onLabelClosing: (openedLabelsNumber) =>
+                        storageUtils.StorageManagerStatic.clearOldTempVariables(openedLabelsNumber),
+                });
                 await importInkText(fileText);
                 // Don't trigger HMR for .ink files, but still allow manual refresh to pick up changes
                 return [];
