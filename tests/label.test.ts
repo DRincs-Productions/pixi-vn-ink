860feb55f9cae7f7c6b53737440fb4d710283c01
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-08-25 19:05:53 +0200

    fix

--- tests/label.test.ts
+++ tests/label.test.ts
@@ -1,33 +1,33 @@
-import { PixiVNJson } from '@drincs/pixi-vn-json';
-import { expect, test } from 'vitest';
-import { convertInkText } from '../src/functions';
+import { PixiVNJson } from "@drincs/pixi-vn-json";
+import { expect, test } from "vitest";
+import { convertInkText } from "../src/functions";
 
-test('Label test 1', async () => {
-	let expected: PixiVNJson = {
-		labels: {
-			back_in_london: [
-				{
-					dialogue: "We arrived into London at 9.45pm exactly.",
-				},
-				{
-					labelToOpen: {
-						label: "hurry_home",
-						type: "jump",
-					},
-				},
-			],
-			hurry_home: [
-				{
-					dialogue: "We hurried home to Savile Row as fast as we could.",
-				},
-				{
-					end: "label_end",
-					goNextStep: true,
-				},
-			]
-		}
-	}
-	let res = convertInkText(`
+test("Label test 1", async () => {
+    let expected: PixiVNJson = {
+        labels: {
+            back_in_london: [
+                {
+                    dialogue: "We arrived into London at 9.45pm exactly.",
+                },
+                {
+                    labelToOpen: {
+                        label: "hurry_home",
+                        type: "jump",
+                    },
+                },
+            ],
+            hurry_home: [
+                {
+                    dialogue: "We hurried home to Savile Row as fast as we could.",
+                },
+                {
+                    end: "label_end",
+                    goNextStep: true,
+                },
+            ],
+        },
+    };
+    let res = convertInkText(`
 === back_in_london ===
 
 We arrived into London at 9.45pm exactly.
@@ -37,32 +37,32 @@ We arrived into London at 9.45pm exactly.
 We hurried home to Savile Row as fast as we could.
 ->DONE
 `);
-	expect(res).toEqual(expected);
+    expect(res).toEqual(expected);
 });
 
-test('Label test 2', async () => {
-	let expected: PixiVNJson = {
-		labels: {
-			back_in_london: [
-				{
-					dialogue: "We arrived into London at 9.45pm exactly.",
-				},
-				{
-					end: "game_end",
-				},
-			],
-			hurry_home: [
-				{
-					dialogue: "We hurried home to Savile Row as fast as we could.",
-				},
-				{
-					end: "label_end",
-					goNextStep: true,
-				},
-			]
-		}
-	}
-	let res = convertInkText(`
+test("Label test 2", async () => {
+    let expected: PixiVNJson = {
+        labels: {
+            back_in_london: [
+                {
+                    dialogue: "We arrived into London at 9.45pm exactly.",
+                },
+                {
+                    end: "game_end",
+                },
+            ],
+            hurry_home: [
+                {
+                    dialogue: "We hurried home to Savile Row as fast as we could.",
+                },
+                {
+                    end: "label_end",
+                    goNextStep: true,
+                },
+            ],
+        },
+    };
+    let res = convertInkText(`
 === back_in_london ===
 
 We arrived into London at 9.45pm exactly.
@@ -72,69 +72,69 @@ We arrived into London at 9.45pm exactly.
 We hurried home to Savile Row as fast as we could.
 ->DONE
 `);
-	expect(res).toEqual(expected);
+    expect(res).toEqual(expected);
 });
 
 /**
  * https://github.com/inkle/ink/blob/master/Documentation/WritingWithInk.md#the-first-stitch-is-the-default
  */
-test('The first stitch is the default', async () => {
-	let expected: PixiVNJson = {
-		labels: {
-			"the_orient_express_|_c-0": [
-				{
-					labelToOpen: {
-						label: "the_orient_express_|_in_first_class",
-						type: "jump",
-					},
-					glueEnabled: undefined,
-				},
-			],
-			"the_orient_express_|_c-1": [
-				{
-					labelToOpen: {
-						label: "the_orient_express_|_in_second_class",
-						type: "jump",
-					},
-					glueEnabled: undefined,
-				},
-			],
-			"the_orient_express_|_in_first_class": [
-				{
-					dialogue: "First class was luxurious.",
-				},
-			],
-			"the_orient_express_|_in_second_class": [
-				{
-					dialogue: "Second class was cramped.",
-				},
-			],
-			the_orient_express: [
-				{
-					dialogue: "We boarded the train, but where?",
-				},
-				{
-					choices: [
-						{
-							text: "First class",
-							label: "the_orient_express_|_c-0",
-							oneTime: true,
-							props: {},
-							type: "jump",
-						},
-						{
-							text: "Second class",
-							label: "the_orient_express_|_c-1",
-							oneTime: true,
-							props: {},
-							type: "jump",
-						},
-					],
-				},
-			],
-		}
-	}
-	let res = convertInkText(`
+test("The first stitch is the default", async () => {
+    let expected: PixiVNJson = {
+        labels: {
+            "the_orient_express_|_c-0": [
+                {
+                    labelToOpen: {
+                        label: "the_orient_express_|_in_first_class",
+                        type: "jump",
+                    },
+                    glueEnabled: undefined,
+                },
+            ],
+            "the_orient_express_|_c-1": [
+                {
+                    labelToOpen: {
+                        label: "the_orient_express_|_in_second_class",
+                        type: "jump",
+                    },
+                    glueEnabled: undefined,
+                },
+            ],
+            "the_orient_express_|_in_first_class": [
+                {
+                    dialogue: "First class was luxurious.",
+                },
+            ],
+            "the_orient_express_|_in_second_class": [
+                {
+                    dialogue: "Second class was cramped.",
+                },
+            ],
+            the_orient_express: [
+                {
+                    dialogue: "We boarded the train, but where?",
+                },
+                {
+                    choices: [
+                        {
+                            text: "First class",
+                            label: "the_orient_express_|_c-0",
+                            oneTime: true,
+                            props: {},
+                            type: "jump",
+                        },
+                        {
+                            text: "Second class",
+                            label: "the_orient_express_|_c-1",
+                            oneTime: true,
+                            props: {},
+                            type: "jump",
+                        },
+                    ],
+                },
+            ],
+        },
+    };
+    let res = convertInkText(`
 === the_orient_express ===
 
 We boarded the train, but where?
@@ -146,63 +146,63 @@ We boarded the train, but where?
 = in_second_class
 	Second class was cramped.
 `);
-	expect(res).toEqual(expected);
+    expect(res).toEqual(expected);
 });
 
 /**
  * https://github.com/inkle/ink/blob/master/Documentation/WritingWithInk.md#local-diverts
  */
-test('Local diverts', async () => {
-	let expected: PixiVNJson = {
-		labels: {
-			"the_orient_express_|_in_first_class_|_c-0": [
-				{
-					labelToOpen: {
-						label: "the_orient_express_|_in_third_class",
-						type: "jump",
-					},
-					glueEnabled: undefined,
-					goNextStep: undefined,
-				},
-			],
-			"the_orient_express_|_in_first_class": [
-				{
-					dialogue: "I settled my master.",
-				},
-				{
-					choices: [
-						{
-							text: "Move to third class",
-							label: "the_orient_express_|_in_first_class_|_c-0",
-							oneTime: true,
-							props: {},
-							type: "jump",
-						},
-					],
-				},
-			],
-			"the_orient_express_|_in_third_class": [
-				{
-					dialogue: "I put myself in third.",
-				},
-				{
-					end: "label_end",
-					goNextStep: true,
-				},
-			],
-			the_orient_express: [
-				{
-					labelToOpen: {
-						label: "the_orient_express_|_in_first_class",
-						type: "jump",
-					},
-					glueEnabled: undefined,
-					goNextStep: undefined,
-				},
-			],
-		}
-	}
-	let res = convertInkText(`
+test("Local diverts", async () => {
+    let expected: PixiVNJson = {
+        labels: {
+            "the_orient_express_|_in_first_class_|_c-0": [
+                {
+                    labelToOpen: {
+                        label: "the_orient_express_|_in_third_class",
+                        type: "jump",
+                    },
+                    glueEnabled: undefined,
+                    goNextStep: undefined,
+                },
+            ],
+            "the_orient_express_|_in_first_class": [
+                {
+                    dialogue: "I settled my master.",
+                },
+                {
+                    choices: [
+                        {
+                            text: "Move to third class",
+                            label: "the_orient_express_|_in_first_class_|_c-0",
+                            oneTime: true,
+                            props: {},
+                            type: "jump",
+                        },
+                    ],
+                },
+            ],
+            "the_orient_express_|_in_third_class": [
+                {
+                    dialogue: "I put myself in third.",
+                },
+                {
+                    end: "label_end",
+                    goNextStep: true,
+                },
+            ],
+            the_orient_express: [
+                {
+                    labelToOpen: {
+                        label: "the_orient_express_|_in_first_class",
+                        type: "jump",
+                    },
+                    glueEnabled: undefined,
+                    goNextStep: undefined,
+                },
+            ],
+        },
+    };
+    let res = convertInkText(`
 -> the_orient_express
 === the_orient_express ===
 = in_first_class
@@ -214,5 +214,5 @@ test('Local diverts', async () => {
 	I put myself in third.
 	->DONE
 `);
-	expect(res).toEqual(expected);
+    expect(res).toEqual(expected);
 });
