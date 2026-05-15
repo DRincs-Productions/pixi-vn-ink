0df334606e120ffcc82a703db56bb61ef2bfc9df
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-05-15 12:21:52 +0200

    fix

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,3 +1,3 @@
 export { HashtagCommands, onInkHashtagScript } from "@/handlers/hashtag-commands";
-export { HashtagHandler } from "@/handlers/interfaces/HashtagHandler";
+export type { HashtagHandler } from "@/handlers/interfaces/HashtagHandler";
 export { TextReplaces } from "@drincs/pixi-vn-json";
