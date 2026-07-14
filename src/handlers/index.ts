a5ab0ee68fd6c92d7a38bf6dfd372bd561baeb1f
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-07-14 19:50:49 +0200

    feat: export additional types from HashtagHandler interface

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,5 +4,9 @@ export {
     onInkHashtagScript,
 } from "@/handlers/hashtag-commands";
 export type { BaseHashtagCommandsOptions } from "@/handlers/hashtag-commands";
-export type { HashtagHandler } from "@/handlers/interfaces/HashtagHandler";
+export type {
+    HashtagHandler,
+    HashtagHandlerOptions,
+    MapperHandler,
+} from "@/handlers/interfaces/HashtagHandler";
 export { TextReplaces } from "@drincs/pixi-vn-json";
